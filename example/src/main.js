a85bf05d7484d6198ae9dd9a87680f816647ca61
= <cm@an.tc>
2016-11-23 18:58:02 -0700

    Add options to the example.

--- example/src/main.js
+++ example/src/main.js
@@ -1,7 +1,18 @@
 chrome.runtime.onMessage.addListener(function (event) {
   if (event.type === 'authenticate') {
+
+    // scope
+    //  - openid if you want an id_token returned
+    //  - offline_access if you want a refresh_token returned
+    // device
+    //  - required if requesting the offline_access scope.
+    let options = {
+      scope: 'openid offline_access',
+      device: 'chrome-extension'
+    };
+
     new Auth0Chrome(env.AUTH0_DOMAIN, env.AUTH0_CLIENT_ID)
-      .authenticate()
+      .authenticate(options)
       .then(function (authResult) {
         localStorage.authResult = JSON.stringify(authResult);
         chrome.notifications.create({
@@ -11,12 +22,12 @@ chrome.runtime.onMessage.addListener(function (event) {
           message: 'You can use the app now'
         });
       }).catch(function (err) {
-        chrome.notifications.create({
-          type: 'basic',
-          title: 'Login Failed',
-          message: err.message,
-          iconUrl: 'icons/icon128.png'
-        });
+      chrome.notifications.create({
+        type: 'basic',
+        title: 'Login Failed',
+        message: err.message,
+        iconUrl: 'icons/icon128.png'
       });
+    });
   }
 });
